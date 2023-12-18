d2d5b87fb4604ffd65311202bba311fe03984206
yser09 <93488977+yser0@users.noreply.github.com>
2023-12-18 13:53:16 +0100

    Delete Document.js

--- Document.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-const document = 'Hello world';
-const hash = crypto.createHash('sha256').update(document).digest('hex');
-
-console.log('Document Hash:', hash);
